c6e520625a31be7f6bfea1083277a65d2cff292d
Jane Sandberg <sandbergja@users.noreply.github.com>
2016-05-31 20:00:18 -0700

    Create open_hours.sql

---
+++ open_hours.sql
@@ -0,0 +1,8 @@
+INSERT INTO open_hours
+(siteid, day, start_time, end_time, by_appointment, private_note)
+VALUES (1, 0, 900, 1700, 0, 'Sample hours'),
+(1, 1, 900, 1700, 0, 'Sample hours'),
+(1, 3, 900, 1700, 0, 'Sample hours'),
+(1, 4, 900, 1700, 0, 'Sample hours'),
+(1, 5, 900, 1200, 1, 'Sample hours'),
+(1, 6, 0, 2359, 1, 'Sample hours');
